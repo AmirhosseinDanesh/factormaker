d5db58459d34167520a3da4c36515720b03b10b4
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-12-27 19:37:35 +0330

    fix bugs

--- src/Pages/Client/ClientFactor.jsx
+++ src/Pages/Client/ClientFactor.jsx
@@ -4,9 +4,9 @@ import React, { useEffect } from 'react'
 export default function ClientFactor() {
     const factorOBJ = JSON.parse(localStorage.getItem("factor"))
     return (
-        <div className='w-[895px] mx-auto mt-20 font-Iransans'>
-            <div className='font-IransansBold text-xl text-center'>{factorOBJ?.factorName}</div>
-            <div className='flex justify-between my-10 p-8 bg-gray-200 rounded-3xl  border-r-4 border-blue-500 font-IransansMedium text-sm'>
+        <div className='w-[895px] mx-auto mt-20 font-Dana'>
+            <div className='font-DanaBold text-xl text-center'>{factorOBJ?.factorName}</div>
+            <div className='flex justify-between my-10 p-8 bg-gray-200 rounded-3xl  border-r-4 border-blue-500 font-DanaMedium text-sm'>
                 <div className='flex justify-between gap-x-2'>
                     <span>صادر کننده:</span>
                     <p>{factorOBJ?.exporter}</p>
@@ -24,32 +24,43 @@ export default function ClientFactor() {
             {/* infoos */}
             <div className='px-4 mt-10 mb-10'>
                 <div className='flex justify-between my-4'>
-                    <span className='font-Iransans text-xs text-gray-500'>توضیحات</span>
+                    <span className='font-Dana text-xs text-gray-500'>توضیحات</span>
                     <span className='bg-gray-400 w-[85%] mt-2 h-px'></span>
-                    <span className='font-Iransans text-xs text-gray-500'>مبلغ</span>
+                    <span className='font-Dana text-xs text-gray-500'>مبلغ</span>
                 </div>
                 <div className='my-4 divide-dotted divide-y-2 divide-gray-100'>
                     {
                         factorOBJ?.infos?.map((info, index) => (
-                            <div key={index} className='flex justify-between py-6 gap-x-16 font-IransansMedium text-sm'>
-                                <div className='flex gap-x-1'>
-                                    <span>{index + 1}.</span>
-                                    <p>{info.infoName}</p>
+                            <div key={index}>
+                                <div className='flex justify-between py-6 gap-x-16 font-DanaMedium text-sm'>
+                                    <div className='flex gap-x-1'>
+                                        <span>{index + 1}.</span>
+                                        <p>{info.infoName}</p>
+
+                                    </div>
+                                    {
+                                        info?.infoPrice === 0 ? "رایگان"
+                                            :
+                                            <div className='flex gap-x-2 flex-shrink-0'>
+                                                <span>{Number(info?.infoPrice).toLocaleString()}</span>
+                                                <span>تومان</span>
+                                            </div>
+                                    }
                                 </div>
-                                <div className='flex gap-x-2 flex-shrink-0'>
-                                    <span>{info?.infoPrice.toLocaleString()}</span>
-                                    <span>تومان</span>
+                                <div className='px-5 py-3 text-gray-800 text-xs leading-6'>
+                                    <span>{info.infoDescription}</span>
                                 </div>
                             </div>
+
                         ))
                     }
                 </div>
                 <div className='flex justify-between my-4'>
                     <span className='bg-gray-400 w-[90%] mt-2 h-px'></span>
-                    <span className='font-IransansBold text-xs text-green-700'>مجموع کل</span>
+                    <span className='font-DanaBold text-xs text-green-700'>مجموع کل</span>
                 </div>
                 <div className='flex justify-end mt-8'>
-                    <span className='flex gap-x-1 items-center font-IransansBold text-base'>
+                    <span className='flex gap-x-1 items-center font-DanaBold text-base'>
                         {
                             Number(factorOBJ?.infos?.reduce((acc, cur) => acc + cur.infoPrice, 0)).toLocaleString()
                         }
@@ -62,11 +73,11 @@ export default function ClientFactor() {
 
 
             <div className='bg-green-200 px-6 py-2 pb-4 mb-4 rounded-2xl'>
-                <div className='flex justify-between font-Iransans text-[10px] text-gray-600 my-8'>
+                <div className='flex justify-between font-Dana text-[10px] text-gray-600 my-8'>
                     <p>اطلاعات حساب برای پرداخت فاکتور</p>
                     <p>مبلغ کلی پرداختی</p>
                 </div>
-                <div className='flex justify-between font-IransansMedium text-sm'>
+                <div className='flex justify-between font-DanaMedium text-sm'>
                     <div className='grid grid-rows-4 grid-flow-col gap-y-6 gap-x-10'>
                         <span className='text-xs'>نام:</span>
                         <span className='text-xs'>شماره کارت:</span>
@@ -77,9 +88,9 @@ export default function ClientFactor() {
                         <span>{factorOBJ?.paymentAccountNumber}</span>
                         <span>{factorOBJ?.paymentIBANNumber}</span>
                     </div>
-                    <div className='flex flex-col gap-y-6 text-xs font-IransansMedium'>
+                    <div className='flex flex-col gap-y-6 text-xs font-DanaMedium'>
                         <div className='flex justify-end gap-x-2 '>
-                            <span className='flex gap-x-1 items-center font-IransansBold text-xs'>
+                            <span className='flex gap-x-1 items-center font-DanaBold text-xs'>
                                 {
                                     Number(factorOBJ?.infos?.reduce((acc, cur) => acc + cur.infoPrice, 0)).toLocaleString()
                                 }
